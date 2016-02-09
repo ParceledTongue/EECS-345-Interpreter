96021e26bd49205e34f6d2db3589233a838b1de9
Jonah Raider Roth <jer135@case.edu>
2016-02-09 15:27:48 -0500

    Added M_value function

--- M_state.scm
+++ M_state.scm
@@ -2,3 +2,30 @@
 (define M_state
   (lambda (statement state)
     state))
+
+(define M_value
+  (lambda (l)
+    (cond
+      ((null? l) 0)
+      ((number? l) l)
+      ((eq? (operator l) '+) (+ (M_value (operand1 l)) (M_value (operand2 l))))
+      ((eq? (operator l) '-) (- (M_value (operand1 l)) (M_value (operand2 l))))
+      ((eq? (operator l) '*) (* (M_value (operand1 l)) (M_value (operand2 l))))
+      ((eq? (operator l) '/) (quotient (M_value (operand1 l)) (M_value (operand2 l))))
+      ((eq? (operator l) '%) (remainder (M_value (operand1 l)) (M_value (operand2 l))))
+      (else (error 'unknown "Unknown expression")))))
+
+; infix notation
+; (define operator cadr)
+; (define operand1 car)
+; (define operand2 caddr)
+
+; prefix notation
+(define operator car)
+(define operand1 cadr)
+(define operand2 caddr)
+
+; postfix notation
+; (define operator caddr)
+; (define operand1 cadr)
+; (define operand2 car)
