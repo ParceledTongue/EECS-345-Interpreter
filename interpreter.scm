652e584ea75816c1cb17652d594df93387455823
Jonah Raider Roth <jer135@case.edu>
2016-02-09 12:25:04 -0500

    Fixed output

--- interpreter.scm
+++ interpreter.scm
@@ -1,10 +1,14 @@
 (load "simpleParser.scm")
 (load "state.scm")
+(load "M_state.scm")
 (load "evaluate.scm")
 
 (define program (parser "tests/test1"))
+
+; the empty state has one value - "return" - which is set to null
 (define empty-state
   (lambda ()
-    '(() ())))
+    '((return) (()))))
 
-(printf (state-get 'return (evaluate program (empty-state))))
+; output the return value of the program
+(state-get 'return (evaluate program (empty-state)))
