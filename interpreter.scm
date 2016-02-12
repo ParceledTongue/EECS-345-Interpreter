1015c4ba2264600fe26e7e3dac1baebbc8b2bd6d
Zachary Palumbo <ztp3@case.edu>
2016-02-12 03:07:59 -0500

    Changed empty-state from a function to a macro for the appropriate list
    Signed-off-by: Zachary Palumbo <ztp3@case.edu>

--- interpreter.scm
+++ interpreter.scm
@@ -6,11 +6,9 @@
 (define program (parser "tests/test1"))
 
 ; the empty state has one value - "return" - which is set to null
-(define empty-state
-  (lambda ()
-    '((return) (()))))
+(define empty-state '((return) (())))
 
 (define sample-state '((a b c x y z q r return) (1 2 3 4 5 6 #t #f '())))
 
 ; output the return value of the program
-(state-get 'return (evaluate program (empty-state)))
+(state-get 'return (evaluate program empty-state))
